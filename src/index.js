2f126d279221d3d9d73353ec0b845e8fcbe6d67c
Tatiana <grabli_77@mail.ru>
2019-02-06 21:27:04 +0300

    codeclimat badge edit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import readlineSync from 'readline-sync';
 
 const helloName = () => {
   const name = readlineSync.question('May I have your name?: ');
-  return console.log(`Hello, ${name}!`);
+  console.log(`Hello, ${name}!`);
+  return name;
 };
 
 export default helloName;
