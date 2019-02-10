77d9e32bb85e71e290388f8f2c2394be29b1e71e
Tatiana <grabli_77@mail.ru>
2019-02-10 21:13:54 +0300

    progression generate and check answer edit

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,35 +1,52 @@
 import game from '../core';
 
-const PROGRESSION_LEHGTH = 10;
-
-
+const progresionLength = 10;
 const description = 'What number is missing in the progression?';
 
 const generateQuestion = () => {
+  const space = +(Math.random() * 10).toFixed();
   const d = (Math.random() * 100).toFixed();
   const firstNum = (Math.random() * 10).toFixed();
-  const space = (Math.random() * 10).toFixed();
-  const createProgression = (i, progressionArray, num) => {
-    if (i <= 0) {
-      return progressionArray;
-    }
-    progressionArray.push(num);
-    return createProgression(i - 1, progressionArray, +num + +d);
-  };
+  const i = 0;
 
-  const progression = createProgression(PROGRESSION_LEHGTH, [], firstNum);
-  progression[space] = '..';
-  return progression.join(' ');
+  const progressionArray = [];
+  progressionArray.push(firstNum);
+
+  for (let i = 1; i < progresionLength + 1; i++) {
+    progressionArray.push(+firstNum + +d * i);
+  }
+  progressionArray[space] = '..';
+  return progressionArray.join(' ');
+
+// const generateProgression = (i, d, firstNum) => {
+//
+// }
+
+
 };
 
 const checkAnswer = (question) => {
   const progression = question.split(' ');
-  if (!Number.isNaN(+progression[progression.length - 1])
-  && !Number.isNaN(+progression[progression.length - 2])) {
-    const result = +progression[progression.length - 1] - +progression[progression.length - 2];
-    return (result).toString();
+  let space; // пропущенный элемент
+  let d;
+  //узнать какой элемент пропущен
+  for (let i = 0; i < progression.length; i++) {
+    if (space && d) {
+      break;
+    }
+    if (progression[i] === '..') {
+      space = i;
+    } else {
+      if (progression[i + 1] !== '..') {
+        d = +progression[i + 1] - +progression[i];
+        // console.log(+progression[i + 2], +progression[i + 1]);
+
+      }
+    }
   }
-  return (+progression[progression.length - 3] - +progression[progression.length - 4]).toString();
+
+  // console.log(+progression[0], d, space);
+  return (+progression[0] + d * space).toString();
 };
 
 const progressionGame = () => game(description, generateQuestion, checkAnswer);
